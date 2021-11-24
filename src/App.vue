d097f40fabf005e9112da24cee10668e2753a6a1
MattiaM99 <mattiamazzoni123@gmail.com>
2021-11-24 12:43:38 +0100

    create le sezioni

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
-    
+    <Header/>
+    <Main/>
+    <Merch/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Merch from './components/Merch.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-   
+   Header,
+   Main,
+   Merch,
+   Footer
   }
 }
 </script>
 
 <style lang="scss">
-
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 </style>
